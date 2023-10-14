e4ec43d069b0d989606afcdec007173e4f5efb94
alokkusingh <alok.ku.singh@gmail.com>
2023-10-14 12:09:22 +0530

    first commit

---
+++ src/main/java/com/alok/aws/s3/refresh/S3RefreshPollerLibApplication.java
@@ -0,0 +1,13 @@
+package com.alok.aws.s3.refresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3RefreshPollerLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3RefreshPollerLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alok/aws/s3/refresh/S3RefreshPollerLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alok.aws.s3.refresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3RefreshPollerLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
